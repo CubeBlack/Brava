e4831ca6e15cb712a86bffb86e93f2050a2fa188
Edson Vitor <edson.vit0r@hotmail.com>
2018-07-16 10:00:55 -0300

    media-menu logo-tamnaho
    Pequena atualização de media do menu, e adição de logos em tamanhos diferentes.

---
+++ _js/js.js
@@ -0,0 +1,10 @@
+/*Menu start*/
+function openSideMenu(){
+  document.getElementById('side-menu').style.width='210px';
+  document.getElementById('main').style.marginLeft='210px';
+}
+function closeSlideMenu(){
+  document.getElementById('side-menu').style.width='0px';
+  document.getElementById('main').style.marginLeft='0px';
+}
+/*Menu end*/
